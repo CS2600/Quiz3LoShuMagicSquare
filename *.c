6f6c439374435a9bdb6ff7177f2bad3307a7a893
jhhocs <joshuaho1@cpp.edu>
2022-10-23 16:10:22 -0700

    Added outline

---
+++ *.c
@@ -0,0 +1,20 @@
+//Lo Shu Magic Square
+
+//Write a function that accepts a two-dimensional array as an argument 
+//and determines whether the array is a Lo Shu Magic Square
+
+
+// 1
+//Test function in main method, 1 success and 1 failure
+
+
+// 2
+//Create counter to keep track of the number of tries until Lo Shu Magic Square is generated
+
+//Create a loop where a 2d array is populated randomly, checking to make sure numbers are not repeated
+
+//Increment counter
+
+//Check if 2d array is an Lo Shu Magic Square
+
+//Repeat untill Lo Shu Magic Square is generated
